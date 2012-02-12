aff18e9bfc51f1abe1ad52844cbbd483010a383b
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2012-02-11 22:19:45 -0500

    initial commit

---
+++ lottery.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+from math import factorial
+
+def ncr(n,r):
+    # n choose r
+    return 1
+
+def solve(M,N,T,P):
+    print M
+    print N
+    print T
+    print P
+    
+
+if __name__ == "__main__":
+    solve(*map(int,sys.stdin.readline().strip().split()))
